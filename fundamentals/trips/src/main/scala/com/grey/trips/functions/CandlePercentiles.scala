5f0740f107a502894ec4888814b07a7d8c091a81
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-07-25 14:40:36 +0100

    Relocate CandlePercentiles.scala ...
    Specifically: fundamentals/trips/src/main/scala/com/grey/functions/CandlePercentiles.scala -> fundamentals/trips/src/main/scala/com/grey/trips/functions/CandlePercentiles.scala

--- fundamentals/trips/src/main/scala/com/grey/trips/functions/CandlePercentiles.scala
+++ fundamentals/trips/src/main/scala/com/grey/trips/functions/CandlePercentiles.scala
@@ -1,4 +1,4 @@
-package com.grey.functions
+package com.grey.trips.functions
 
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
@@ -14,8 +14,8 @@ class CandlePercentiles(spark: SparkSession) extends CandlePoints {
 
   /**
     *
-    * @param records: the frame of records
-    * @param partition: The frame's epoch time field
+    * @param records: The frame of records
+    * @param partition: The field that determines the partition within which a quantile is calculated
     * @param field: The field whose quantiles will be calculated
     * @return
     */
@@ -27,17 +27,22 @@ class CandlePercentiles(spark: SparkSession) extends CandlePoints {
     //    encoders for most common types, which are automatically provided by importing spark.implicits._
     import spark.implicits._
 
-    // The distinct epoch dates/times
+    // The distinct partitions
     val partitions = records.select(col(partition))
       .distinct().map(x => x.getAs[Long](partition)).collect()
 
     // The quantiles, etc.
     val tiles: ParArray[Candle] = partitions.par.map{ x =>
+
       val data: Dataset[Row] = records.filter(col(partition) === x)
-      val quantiles = data.stat.approxQuantile(col = field, probabilities = Array(0.1, 0.25, 0.5, 0.75, 0.9), relativeError = 0)
+      val quantiles = data.stat.approxQuantile(col = field,
+        probabilities = Array(0.1, 0.25, 0.5, 0.75, 0.9), relativeError = 0)
 
       Candle(x, quantiles(0), quantiles(1), quantiles(2), quantiles(3), quantiles(4), data.count())
+
     }
+
+    // Switch to print ...
     tiles.toArray
 
   }
