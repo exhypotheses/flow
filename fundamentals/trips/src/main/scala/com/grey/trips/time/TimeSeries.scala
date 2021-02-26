a76dfc6b66ca8d2f42cd734e861cc3a3f69e9830
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-26 14:25:14 +0000

    Update fundamentals/trips/src/main/scala/com/grey/trips/time/TimeSeries.scala

--- fundamentals/trips/src/main/scala/com/grey/trips/time/TimeSeries.scala
+++ fundamentals/trips/src/main/scala/com/grey/trips/time/TimeSeries.scala
@@ -1,8 +1,14 @@
 package com.grey.trips.time
 
-import org.joda.time.DateTime
+import org.joda.time.{DateTime, Period}
+import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
 
 class TimeSeries {
+
+  val dateTimeNow: DateTime = DateTime.now
+  val dateTimeFormatter: DateTimeFormatter = DateTimeFormat.forPattern("yyyy/MM/dd")
+  val endingString: String = dateTimeFormatter.print(dateTimeNow.minus(Period.days(1)))
+  val endingDateTime: DateTime = dateTimeFormatter.parseDateTime(endingString)
 
   def timeSeries(from: DateTime, until: DateTime, step: Int, stepType: String): List[DateTime] = {
 
@@ -10,7 +16,6 @@ class TimeSeries {
       case "days" =>
         Iterator.iterate(from)( x => x.plusDays(step)).takeWhile(x => !x.isAfter(until) )
       case "months" =>
-
         Iterator.iterate(from)( x => x.plusMonths(step)).takeWhile(x => !x.isAfter(until) )
       case _ =>
         sys.error("Unknown Step Type")
