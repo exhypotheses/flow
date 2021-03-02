3816bf67757a6cdc00707c02f32013d088f447a1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-03-02 15:34:07 +0000

    Rename .../grey/trips/functions/DataReset.scala -> .../grey/trips/src/Transfer.scala: Specifically ...
    	fundamentals/trips/src/main/scala/com/grey/trips/functions/DataReset.scala ->
    	fundamentals/trips/src/main/scala/com/grey/trips/src/Transfer.scala

--- fundamentals/trips/src/main/scala/com/grey/trips/src/Transfer.scala
+++ fundamentals/trips/src/main/scala/com/grey/trips/src/Transfer.scala
@@ -1,4 +1,4 @@
-package com.grey.trips.functions
+package com.grey.trips.src
 
 import java.io.File
 import java.nio.file.{Files, Path, Paths, StandardCopyOption}
@@ -9,25 +9,22 @@ import scala.collection.parallel.mutable.ParArray
 import scala.util.Try
 import scala.util.control.Exception
 
-class DataReset {
+class Transfer {
 
   private val dataDirectories = new DataDirectories()
 
-  def dataReset(source: String, destination: String): ParArray[Try[Path]] = {
-
+  def transfer(source: String, destination: String): ParArray[Try[Path]] = {
 
     // Ensure the destination directory is empty
     val directory: Try[Unit] = Exception.allCatch.withTry(
       dataDirectories.localDirectoryReset(destination)
     )
 
-
     // Get the array of files of interest
     val filesObject = new File(source)
     val arrayOfFiles: Array[File] = filesObject.listFiles.filter(_.getName.endsWith(".csv"))
 
-
-    // Move files
+    // Transfer files
     if (directory.isSuccess) {
       arrayOfFiles.par.map{file =>
         val relocate: Try[Path] = Exception.allCatch.withTry(Files.move(Paths.get(file.toString),
