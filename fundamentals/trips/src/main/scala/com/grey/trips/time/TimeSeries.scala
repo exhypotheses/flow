34f5f939a0723473568f0c10dfe481a4a1733d4f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-03-14 00:26:50 +0000

    Update fundamentals/trips/src/main/scala/com/grey/trips/time/TimeSeries.scala

--- fundamentals/trips/src/main/scala/com/grey/trips/time/TimeSeries.scala
+++ fundamentals/trips/src/main/scala/com/grey/trips/time/TimeSeries.scala
@@ -1,14 +1,8 @@
 package com.grey.trips.time
 
-import org.joda.time.{DateTime, Period}
-import org.joda.time.format.{DateTimeFormat, DateTimeFormatter}
+import org.joda.time.DateTime
 
 class TimeSeries {
-
-  val dateTimeNow: DateTime = DateTime.now
-  val dateTimeFormatter: DateTimeFormatter = DateTimeFormat.forPattern("yyyy/MM/dd")
-  val endingString: String = dateTimeFormatter.print(dateTimeNow.minus(Period.days(1)))
-  val endingDateTime: DateTime = dateTimeFormatter.parseDateTime(endingString)
 
   def timeSeries(from: DateTime, until: DateTime, step: Int, stepType: String): List[DateTime] = {
 
