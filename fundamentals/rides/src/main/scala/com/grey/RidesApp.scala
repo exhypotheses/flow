61a4258354034c7072998960c835574e6e54a44b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-03-17 18:53:40 +0000

    Add fundamentals/rides/src/main/scala/com/grey/RidesApp.scala

---
+++ fundamentals/rides/src/main/scala/com/grey/RidesApp.scala
@@ -0,0 +1,47 @@
+package com.grey
+
+import com.grey.environment.{DataDirectories, LocalSettings}
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.SparkSession
+
+import scala.collection.parallel.immutable.ParSeq
+import scala.util.Try
+
+object RidesApp {
+
+  private val localSettings = new LocalSettings()
+
+  def main(args: Array[String]): Unit = {
+
+    // Minimising log information output
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("aka").setLevel(Level.OFF)
+
+
+    // Spark Session
+    val spark = SparkSession.builder().appName("stations")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .config("spark.sql.warehouse.dir", localSettings.warehouseDirectory)
+      .getOrCreate()
+
+
+    // Prepare local directories
+    val dataDirectories = new DataDirectories()
+    val directories: ParSeq[Try[Boolean]] = List(localSettings.dataDirectory, localSettings.warehouseDirectory)
+      .par.map( directory => dataDirectories.localDirectoryReset(directory) )
+
+
+    // Proceed
+    if (directories.head.isSuccess) {
+
+    } else {
+      sys.error(directories.head.failed.get.getMessage)
+    }
+
+
+
+
+
+  }
+
+}
