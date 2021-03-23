8f90142d978ba76faac6190475fad174921675e1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-03-23 22:19:30 +0000

    Add fundamentals/rides/src/main/scala/com/grey/environment/ConfigurationParameters.scala

---
+++ fundamentals/rides/src/main/scala/com/grey/environment/ConfigurationParameters.scala
@@ -0,0 +1,16 @@
+package com.grey.environment
+
+class ConfigurationParameters {
+
+  /**
+    * Spark Configurations Parameters
+    * If
+    *    - m4.2xLarge Master Node: multiples of 16 because there are 16 cores available
+    *    - a local 8 core machine: multiples of 8
+    * sql.shuffle.partitions: The number of shuffle partitions for joins & aggregation
+    * default.parallelism: The default number of partitions delivered after a transformation
+    */
+  val nShufflePartitions = 2
+  val nParallelism = 2
+
+}
