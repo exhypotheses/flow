21d899b373252872dbe034a947f02177a5d22881
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-03-24 21:56:42 +0000

    Add fundamentals/rides/src/main/scala/com/grey/source/InterfaceTimeSeries.scala

---
+++ fundamentals/rides/src/main/scala/com/grey/source/InterfaceTimeSeries.scala
@@ -0,0 +1,87 @@
+package com.grey.source
+
+import com.grey.time.{TimeFormats, TimeSequences, TimeSeries}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+
+import scala.util.Try
+import scala.util.control.Exception
+
+/**
+  *
+  * @param spark: A SparkSession instance
+  */
+class InterfaceTimeSeries(spark: SparkSession) {
+
+
+  // Today
+  private val dateTimeNow: DateTime = DateTime.now
+
+
+  // Starting
+  private val isTable: Try[DataFrame] = Exception.allCatch.withTry(
+    new com.grey.libraries.mysql.UnloadData(spark = spark)
+      .unloadData(queryString = "SELECT DATE_FORMAT(MAX(start_date), '%Y/%m') as start, MAX(start_date) as filter from rides",
+        databaseString = "mysql.flow")
+  )
+
+
+  /**
+    *
+    * @param interfaceVariables: A class of the source's key variables
+    * @return
+    */
+  def interfaceTimeSeries(interfaceVariables: InterfaceVariables): List[DateTime] = {
+
+
+    // Lower Boundary
+    val (start, filter): (String, String) = if (isTable.isSuccess) {
+      (isTable.get.head().getAs[String]("start"),
+        isTable.get.head().getAs[String]("filter"))
+    } else {
+      ("", "")
+    }
+
+    val startDate: String = if (start.isEmpty) {
+      interfaceVariables.variable("times", "startDate")
+    } else {
+      start
+    }
+
+
+    // Upper Boundary
+    val endDate: String = if (interfaceVariables.variable("times", "endDate").isEmpty) {
+      DateTimeFormat.forPattern(interfaceVariables.dateTimePattern).print(dateTimeNow)
+    } else {
+      interfaceVariables.variable("times", "endDate")
+    }
+
+
+    // The DateTime forms of the start/from & end/until dates
+    val timeFormats = new TimeFormats(interfaceVariables.dateTimePattern)
+    val from: DateTime = timeFormats.timeFormats(startDate)
+    val until: DateTime = timeFormats.timeFormats(endDate)
+
+
+    // Is from prior to until?
+    new TimeSequences().timeSequences(from = from, until = until)
+
+
+    // Hence, the list of dates
+    val timeSeries = new TimeSeries()
+    val listOfDates: Try[List[DateTime]] = Exception.allCatch.withTry(
+      timeSeries.timeSeries(from, until, interfaceVariables.step, interfaceVariables.stepType)
+    )
+
+    if (listOfDates.isSuccess){
+      listOfDates.get.distinct
+    } else {
+      sys.error(listOfDates.failed.get.getMessage)
+    }
+
+
+  }
+
+
+}
