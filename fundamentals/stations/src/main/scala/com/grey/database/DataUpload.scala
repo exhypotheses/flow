c81d5eff9494c7251a26acbd2c1e5b643b494315
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-03-31 00:44:03 +0100

    Update fundamentals/stations/src/main/scala/com/grey/database/DataUpload.scala: In preparation for a database type switch.

--- fundamentals/stations/src/main/scala/com/grey/database/DataUpload.scala
+++ fundamentals/stations/src/main/scala/com/grey/database/DataUpload.scala
@@ -1,7 +1,7 @@
 package com.grey.database
 
-import com.grey.environment.{DataDirectories, LocalSettings}
-import com.grey.libraries.mysql.LoadData
+
+import com.grey.libraries.postgresql.CopyData
 
 import scala.util.Try
 
@@ -11,29 +11,28 @@ import scala.util.Try
   */
 class DataUpload() {
 
-  private val localSettings = new LocalSettings()
+  // private val localSettings = new LocalSettings()
 
   /**
     *
-    * @param tableVariables: A Map of variables for creating & updating tables
+    * @param tableVariables : A Map of variables for creating & updating tables
     */
   def dataUpload(tableVariables: Map[String, String]): Try[Boolean] = {
 
     // Uploading
-    val loadData = new LoadData()
-    val upload: Try[Boolean] = loadData.loadData(databaseString = "mysql.flow",
+    val copyData = new CopyData()
+    val copy: Try[Boolean] = copyData.copyData(databaseString = "postgresql.flow",
       tableVariables = tableVariables)
 
+
     // Hence
-    if (upload.isSuccess) {
-
-      new DataDirectories()
-        .localDirectoryDelete(directoryName = localSettings.warehouseDirectory)
-
-      upload
+    if (copy.isSuccess) {
+      // new DataDirectories()
+      //   .localDirectoryDelete(directoryName = localSettings.warehouseDirectory)
+      copy
     } else {
       // Superfluous
-      sys.error(upload.failed.get.getMessage)
+      sys.error(copy.failed.get.getMessage)
     }
 
   }
