89f11bd643081916278b738944cd6b8ea1a3efab
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-03-31 22:55:10 +0100

    Add fundamentals/rides/src/main/scala/com/grey/database/DataUpload.scala

---
+++ fundamentals/rides/src/main/scala/com/grey/database/DataUpload.scala
@@ -0,0 +1,41 @@
+package com.grey.database
+
+import com.grey.environment.LocalSettings
+import com.grey.libraries.postgresql.CopyData
+
+import scala.util.Try
+
+
+/**
+  * Upload
+  */
+class DataUpload() {
+
+  private val localSettings = new LocalSettings()
+
+  /**
+    *
+    * @param tableVariables : A Map of variables for creating & updating tables
+    */
+  def dataUpload(tableVariables: Map[String, String]): Try[Boolean] = {
+
+    // Uploading
+    val copyData = new CopyData()
+    val copy: Try[Boolean] = copyData.copyData(databaseString = localSettings.databaseString,
+      tableVariables = tableVariables)
+
+
+    // Hence
+    if (copy.isSuccess) {
+      // new DataDirectories()
+      //   .localDirectoryDelete(directoryName = localSettings.warehouseDirectory)
+      copy
+    } else {
+      // Superfluous
+      sys.error(copy.failed.get.getMessage)
+    }
+
+  }
+
+
+}
