d1f35aa78b50287b25a5c18e5ad6796c0434f286
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-04-02 18:03:24 +0100

    Update fundamentals/rides/src/main/scala/com/grey/database/DataStartUp.scala

---
+++ fundamentals/rides/src/main/scala/com/grey/database/DataStartUp.scala
@@ -0,0 +1,69 @@
+package com.grey.database
+
+import com.grey.environment.LocalSettings
+import com.grey.libraries.postgresql.UnloadData
+import com.grey.source.InterfaceVariables
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import scala.util.Try
+
+
+/**
+  *
+  * @param spark: An instance of SparkSession
+  */
+class DataStartUp(spark: SparkSession) {
+
+  private val unloadData = new UnloadData(spark = spark)
+  private val localSettings = new LocalSettings()
+
+
+  /**
+    *
+    * @return The number of records in 'rides', at present
+    */
+  def shape(): Long = {
+
+    // Number of records
+    unloadData.unloadData(queryString = s"SELECT COUNT(*) AS n FROM rides",
+      databaseString = localSettings.databaseString).get.head.getAs[Long]("n")
+  }
+
+
+  /**
+    *
+    * @param interfaceVariables: Interface variables
+    *
+    * @return The starting dates strings
+    */
+  def dates(interfaceVariables: InterfaceVariables): (String, String) = {
+
+    // Lower Boundary
+    if (shape() > 0) {
+
+      val isTable: Try[DataFrame] = unloadData.unloadData(
+        queryString = "SELECT to_char(MAX(start_date), 'YYYY/MM') as start, MAX(start_date) as filter from rides",
+        databaseString = localSettings.databaseString)
+
+      (isTable.get.head().getAs[String]("start"),
+        isTable.get.head().getAs[String]("filter"))
+
+    } else {
+      (interfaceVariables.variable("times", "startDate"),
+        interfaceVariables.variable("times", "filterString"))
+    }
+
+  }
+
+
+  /**
+    *
+    * @return The stations
+    */
+  def stations(): DataFrame = {
+    unloadData.unloadData(queryString = "SELECT station_id FROM stations",
+      databaseString = localSettings.databaseString).get
+  }
+
+
+}
