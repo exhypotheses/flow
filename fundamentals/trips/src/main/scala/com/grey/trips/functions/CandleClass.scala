878a3fd5dd8c398013f6b01e83fa9ce917e24895
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-01 22:02:35 +0100

    Archived: fundamentals/trips/

--- fundamentals/trips/src/main/scala/com/grey/trips/functions/CandleClass.scala
+++
@@ -1,8 +0,0 @@
-package com.grey.trips.functions
-
-class CandleClass {
-
-  case class Candle(epoch: Long, lowerWhisker: Double, lowerQuartile: Double,
-                    median: Double, upperQuartile: Double, upperWhisker: Double, tally: Long)
-
-}
